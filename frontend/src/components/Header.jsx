9d186a84dd1e851544280ae1625421f9c44d7f6d
Himanshu <mehra3228@gmail.com>
2023-04-07 00:40:45 +0530

    front end complete=> redux setup

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+// import { logout } from "../features/auth/authSlice";
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">Support Desk</Link>
+      </div>
+      <ul>
+        {/* {user ? (
+          <li>
+            <button className='btn'>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to='/login'>
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to='/register'>
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )} */}
+        <li>
+          <Link to="/login">
+            <FaSignInAlt /> Login
+          </Link>
+        </li>
+        <li>
+          <Link to="/register">
+            <FaUser /> Register
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 function Login() {
+  const [formdata, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const {email, password } = formdata;
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+   
+  };
   return (
-    <div>
-      Login
-    </div>
-  )
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please Login to get support</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              onChange={onChange}
+              placeholder="Enter your e-mail"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              onChange={onChange}
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button className="btn btn-block">Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 }
 
-export default Login
+export default Login;
